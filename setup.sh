4d3a8a42f5fc39df88973b89fdc3be29c1860333
Wessel Smit <wrsmit00@gmail.com>
2020-07-31 23:48:47 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# This script removes all boilerplate specific code and creates a new package.json.
+# Warning: this script is meant to only be run once and will automatically remove itself.
+
+# Overwrite the package.json with the template package.json
+mv package.template.json package.json
+echo "Deleted package.template.json and created package.json"
+
+# # Setup fresh git history
+# rm -fr .git
+# git init
+
+# Replace placeholder project-name with passed 
+if [ $1 ]; then
+    sed -i "" "s/project-name/$1/g" "package.json"
+    echo "Set project name to $1."
+fi
+
+# Install dependencies
+npm install
+echo "Installed npm dependencies."
+
+# Remove setup.sh 
+rm setup.sh
+echo "Setup done."
