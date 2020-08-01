fd0899fc38c12915ace72267e2154d018dfc451f
Wessel Smit <wrsmit00@gmail.com>
2020-08-01 22:24:39 +0200

    create clean readme.md

--- init.sh
+++ init.sh
@@ -4,18 +4,22 @@
 
 # Overwrite the package.json with the template package.json
 mv package.template.json package.json
-echo "Deleted package.template.json and created package.json"
+echo "[boilerplate] Deleted package.template.json and created package.json."
 
 # Replace placeholder project-name with passed 
 if [ $1 ]; then
     sed -i "" "s/project-name/$1/g" "package.json"
-    echo "Set project name to $1."
+    echo "[boilerplate] Set project name to $1 in package.json."
 fi
+
+# Reset README.md
+echo "" > README.md
+echo "[boilerplate] reset readme.md."
 
 # Install dependencies
 npm install
-echo "Installed npm dependencies."
+echo "[boilerplate] Installed npm dependencies."
 
-# Remove setup.sh 
+# Remove init.sh 
 rm init.sh
-echo "Setup done."
+echo "[boilerplate] Setup done."
